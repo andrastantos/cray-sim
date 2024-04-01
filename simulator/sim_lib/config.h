cc59ad4441b62c5ce961b10c9b5d04db29308e29
Kevin Jordan <kej@kyrafre.com>
2024-04-01 12:12:49 -0400

    Don't abort the simulator when a program attempts to execute an illegal or unimplemented instruction

--- simulator/sim_lib/config.h
+++ simulator/sim_lib/config.h
@@ -2,7 +2,7 @@
 #define __CONFIG_H__
 
 //#define CRAY_TURBO // If enabled, removes all logging capability
-//#define NO_ASSERTS // If enabled, removes all asserts from the code, by defining CRAY_ASSERT as empty
+#define NO_ASSERTS // If enabled, removes all asserts from the code, by defining CRAY_ASSERT as empty
 //#define SINGLE_THREADED // If enabled, compiles code for single-threaded use only (multi-threaded builds are probably broken by now)
 #define PARTIAL_DEBUG // If enabled, turns off optimization for certain functions
 
